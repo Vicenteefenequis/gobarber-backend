6efd52da4f25ceb50179401f044f5235f6b47b05
Vicenteefenequis <vicente19981@live.com>
2020-08-13 20:58:13 -0300

    updated gobarber

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '0e7088d8f08d7a39d1675aa518d2abd1',
+    expersIn: '1d',
+  },
+};
--- src/database/migrations/1596985405551-CreateUsers.ts
+++ src/database/migrations/1596985405551-CreateUsers.ts
@@ -1,27 +1,30 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1596406155548 implements MigrationInterface {
+export default class CreateUsers1596985405551 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
-            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -39,17 +42,6 @@ export class CreateAppointments1596406155548 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
-
-/**
- * Linha do Tempo
- *
- *
- * 1 Semana : Agendamentos
- * 2 Semana : Usuários
- * (Novo Dev) 3 Semana Edição Agendamentos
- * 4 Semana : Compras
- *
- */
